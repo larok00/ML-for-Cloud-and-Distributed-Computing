58e57c3a405a2cb95a70bafd974efc4e7bbc0c1a
Koral Hassan <koraluludag1@hotmail.com>
2019-05-19 20:25:33 +0100

    Can calculate spatial correlations but getting values larger than 1.

--- dataset_intro.py
+++ dataset_intro.py
@@ -46,7 +46,9 @@ plt.show()
 #%%
 def ccf(x, y, lag_max = 100):
 
-    result = ss.correlate(y - np.mean(y), x - np.mean(x), method='direct') / (np.std(y) * np.std(x) * len(y))
+    result = (
+        ss.correlate(y - np.mean(y), x - np.mean(x), method='direct')
+        / (np.std(y) * np.std(x) * len(y)))
     length = (len(result) - 1) // 2
     lo = length - lag_max
     hi = length + (lag_max + 1)
@@ -54,20 +56,24 @@ def ccf(x, y, lag_max = 100):
     return result[lo:hi]
 
 #%%
-sample_size = 10
+sample_size = 500
+cpu_sample = cpu_data[:sample_size]
+mem_sample = mem_data[:sample_size]
 correlations = np.empty((sample_size, sample_size))
-i = j = 0
-for machine_x in cpu_data[:sample_size]:
-    for machine_y in cpu_data[:sample_size]:
-        correlations[i, j] = ccf(machine_x, machine_y, lag_max=0)
-        j++
-    i++
+for i in range(sample_size):
+    k=0
+    for j in range(sample_size):
+        machine_x = cpu_data[i][:1000]
+        machine_y = cpu_data[j][:1000]
+        if i != j:
+            correlations[i, k] = ccf(machine_x, machine_y, lag_max=0)
+            k += 1
+    i += 1
 
 #%%
-result.shape
+print(correlations)
+print(np.amax(correlations))
+print(np.amin(correlations))
 
-
-#%%
-print(result)
 
 #%%
