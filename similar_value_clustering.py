7f8c60118bc2eb7d7be169c2624b7b841074eeb5
Koral Hassan <koraluludag1@hotmail.com>
2019-05-22 15:54:41 +0100

    I think clustering for similar values works.

--- similar_value_clustering.py
+++ similar_value_clustering.py
@@ -5,6 +5,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.signal as ss
+import time
+
 np.random.seed(19)
 
 #%%
@@ -62,7 +64,10 @@ plt.tight_layout()
 plt.show()
 
 #%%
-UTILISATION_BINS = {(i/10, i/10+0.1) for i in range(10)}
+NO_OF_BINS = 10
+BIN_INTERVAL = 1 / NO_OF_BINS
+UTILISATION_BINS = {(i / NO_OF_BINS,
+                     i / NO_OF_BINS + BIN_INTERVAL) for i in range(10)}
 
 # 5 minutes,
 # 10 minutes,
@@ -71,16 +76,131 @@ UTILISATION_BINS = {(i/10, i/10+0.1) for i in range(10)}
 # 1 day,
 # 1 week.
 # Unit of time in timestamps is 5 minutes.
-TREND_LENGTHS = [1, 2, 3, 60//5, 1*DAYS_TO_MINUTES//5, 7*DAYS_TO_MINUTES//5]
-TRENDS = {'INCRESING', 'STABLE', 'DECREASING'}
+TREND_LENGTHS = [1, 2, 3, 60 // 5, 1 * 24 * 60 // 5, 7 * 24 * 60 // 5]
+TREND_VALS = {'INCREASING', 'STABLE', 'DECREASING'}
 
-for data_type in TEMPORAL_CORRELATIONS:
-    correlations = TEMPORAL_CORRELATIONS[data_type]
-    for i in range(SPATIAL_SAMPLE_SIZE):
-        for j in range(NO_OF_TIMESTAMPS):
-            break
-            bin = None
-            correlations[i, j] = None
+class Cluster(object):
+    def __init__(self, bin_trends_tup):
+        self.bin = bin_trends_tup[0]
+        self.trends = bin_trends_tup[1]
+        self.members = []
+        self.average = None
+    
+    def add_member(self, new_member):
+        self.members.append(new_member)
+        cluster_size = len(self.members)
+        if self.average is None:
+            self.average = new_member
+        else:
+            self.average = (((cluster_size-1) * self.average + new_member)
+                            / cluster_size)
 
+class ClusterMember(object):
+    def __init__(self, data_type, machine_no, t):
+        corr = TEMPORAL_CORRELATIONS[data_type]
+        self.value = corr[machine_no, t]
+
+        lo = self.value // BIN_INTERVAL / NO_OF_BINS
+        hi = lo + BIN_INTERVAL
+        self.bin = (lo, hi)
+        assert self.bin in UTILISATION_BINS, '{} is not in {}'.format(
+            self.bin, UTILISATION_BINS)
+        self.previous_values = dict()
+        self.percent_changes = dict()
+        self.trends = dict()
+
+        for length in TREND_LENGTHS:
+            if length > t:
+                previous_value = None
+                percent_change = None
+                trend = 'STABLE'
+            else:
+                previous_value = corr[machine_no, t - length]
+
+                if previous_value == 0.0:
+                    percent_change = -1.0
+                else:
+                    percent_change = self.value / previous_value - 1
+
+                if percent_change > 1/3:
+                    trend = 'INCREASING'
+                elif percent_change < -1/3:
+                    trend = 'DECREASING'
+                else:
+                    trend = 'STABLE'
+            assert trend in TREND_VALS, '{} is not in {}'.format(trend,
+                                                                 TREND_VALS)
+
+            self.previous_values[length] = previous_value
+            self.percent_changes[length] = percent_change
+            self.trends[length] = trend
+
+CLUSTERS = dict()
 
 #%%
+start = time.process_time()
+CORR_CLUSTERINGS = dict()
+for data_type in TEMPORAL_CORRELATIONS:
+    corr = TEMPORAL_CORRELATIONS[data_type]
+    CORR_CLUSTERINGS[data_type] = np.empty(corr.shape, dtype=tuple)
+    for machine_no in range(SPATIAL_SAMPLE_SIZE):
+        for t in range(NO_OF_TIMESTAMPS):
+            lo = corr[machine_no, t] // BIN_INTERVAL / NO_OF_BINS
+            hi = lo + BIN_INTERVAL
+            bin = (lo, hi)
+            assert bin in UTILISATION_BINS, '{} is not in {}'.format(
+                bin, UTILISATION_BINS)
+
+            trends = list()
+            for length in TREND_LENGTHS:
+                if length > t:
+                    trend = 'STABLE'
+                else:
+                    if corr[machine_no, t - length] == 0.0:
+                        percent_change = 1.0
+                    else:
+                        percent_change = (corr[machine_no, t]
+                                        / corr[machine_no, t - length]
+                                        - 1)
+
+                    if percent_change > 1/3:
+                        trend = 'INCREASING'
+                    elif percent_change < -1/3:
+                        trend = 'DECREASING'
+                    else:
+                        trend = 'STABLE'
+
+                assert trend in TREND_VALS, '{} is not in {}'.format(
+                    trend, TREND_VALS)
+                trends.append(trend)
+
+            CORR_CLUSTERINGS[data_type][machine_no, t] = (bin, trends)
+elapsed_time = time.process_time() - start
+
+#%%
+start = time.process_time()
+for data_type in TEMPORAL_CORRELATIONS:
+    corr = TEMPORAL_CORRELATIONS[data_type]
+    clusters = CORR_CLUSTERINGS[data_type]
+    for machine_no in range(SPATIAL_SAMPLE_SIZE):
+        for t in range(NO_OF_TIMESTAMPS):
+            cluster = clusters[machine_no, t]
+            if cluster not in CLUSTERS:
+                CLUSTERS[cluster] = Cluster(cluster)
+            CLUSTERS[cluster].add_member()
+
+#%%
+start = time.process_time()
+CORR_CLUSTERINGS = dict()
+for data_type in TEMPORAL_CORRELATIONS:
+    corr = TEMPORAL_CORRELATIONS[data_type]
+    CORR_CLUSTERINGS[data_type] = np.empty(corr.shape, dtype=tuple)
+    for machine_no in range(SPATIAL_SAMPLE_SIZE):
+        for t in range(NO_OF_TIMESTAMPS):
+            member = ClusterMember(data_type, machine_no, t)
+
+            CORR_CLUSTERINGS[data_type][machine_no, t] = (bin, trends)
+elapsed_time = time.process_time() - start
+
+#%%
+elapsed_time
